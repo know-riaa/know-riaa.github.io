d31fb9289d56096170ddb6ad66e2f528654b9c19
know-riaa <knowriaa@gmail.com>
2026-01-19 00:50:25 -0500

    PAges for mobile view

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div className='pt-24 pl-20 font-big text-[13rem] leading-[13rem]'>
+      <div className='font-big pt-[14rem] pl-[2rem] text-[2rem] items-center lg:pt-24 lg:pl-20  lg:text-[12rem] lg:leading-[13rem]'>
         RIA THAPA.<br></br>
         FULL STACK ENGINEER.<br></br>
         <span className='text-brand-red'>AI ORCHESTRATIONS.</span>
@@ -17,7 +17,7 @@ function App() {
       <a
         href='https://www.linkedin.com/in/riathapa/'
         target='_blank' 
-        className='block pt-12 font-big text-[4rem] pl-20
+        className='w-fit font-big text-[1.2rem] pl-[2rem] block lg:pt-12 lg:text-[4rem] lg:pl-20
        hover:text-brand-red hover:underline'>LINKED IN</a>
     </>
   );
