0db99a3f645bc148becaa5bc31d2f1906a97ac8d
know-riaa <knowriaa@gmail.com>
2026-03-17 00:26:46 -0400

    Huh, les go, fixed the middle of the backend area

--- portfolio/src/pages/Hero.jsx
+++ portfolio/src/pages/Hero.jsx
@@ -18,11 +18,10 @@ function Hero(){
         <motion.div ref={containerRef} className='relative min-h-screen overflow-hidden text-white'>
           <motion.div 
             style={{ y:nameY, scale:nameScale }}
-            className='flex items-center justify-center h-screen'>
-              <div className='text-center'>
-                <div className='text-5xl md:text-9xl font-big leading-none'>
-                  <Link to='/about'
-                    className='hover:animate-pulse hover:text-red-500'>
+            className='flex items-center justify-center h-screen hover:animate-pulse hover:text-red-500'>
+              <div className='text-center hover:animate-pulse hover:text-red-500'>
+                <div className='text-5xl md:text-9xl font-big'>
+                  <Link to='/about' className='inline-block m-0'>
                     RIA THAPA.
                   </Link>
                 </div>
@@ -31,7 +30,7 @@ function Hero(){
         </motion.div>
 
         <motion.div style={{ opacity: roleOpacity}}
-          className='fixed top-64 md:top-1/2 md:-translate-y-1/2 left-0 right-0 z-40 backdrop-blur-sm bg-black/50'>
+          className='fixed top-60 md:top-72 left-0 right-0 z-40 backdrop-blur-sm bg-black/50'>
             <div className='text-4xl md:text-9xl font-big leading-normal text-center'>
               <span className='text-white'>BACKEND ENGINEER</span>
               <br></br>
@@ -41,7 +40,7 @@ function Hero(){
         </motion.div>
 
         <motion.div style={{ opacity:linksOpacity}}
-        className='fixed bottom-60 left-1/2 -translate-x-1/2 md:bottom-54 flex gap-8 md:gap-28 font-big text-2xl md:text-6xl w-fit'>
+        className='fixed bottom-64 left-1/2 -translate-x-1/2 md:bottom-80 flex gap-8 md:gap-28 font-big text-2xl md:text-6xl w-fit pointer-events-none'>
           <a href='https://www.linkedin.com/in/riathapa/' target='_blank' className=' 
         hover:text-red-500 hover:underline hover:underline-offset-4
         ' rel='noopener noreferrer'>
